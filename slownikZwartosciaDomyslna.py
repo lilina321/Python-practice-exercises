3cc0319e50c5bc918d3e326093e3e82e94f32409
Lilina Lee <321lilina321@gmail.com>
2021-02-25 09:00:28 +0100

    slownik z wartoscia domyslna - defaultdict

---
+++ slownikZwartosciaDomyslna.py
@@ -0,0 +1,10 @@
+'''Słownik z wartością domyślną
+stosowanie: uniknąć KeyError'''
+
+from collections import defaultdict
+
+a = defaultdict(int) # 0
+
+b = defaultdict(float) # 0.0
+
+c = defaultdict(str) # ''
